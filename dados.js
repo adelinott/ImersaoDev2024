e5798fce2e9e533dba98c518b9cc56d025fa4d2d
adelinott <adelinott@gmail.com>
2024-09-19 09:45:17 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,32 @@
+let dados = [
+    {
+        titulo: "Johann Sebastian Bach",
+        descricao: "Johann Sebastian Bach (1685-1750): Considerado o pai da música barroca, Bach era um mestre da polifonia e da harmonia. Suas obras são conhecidas por sua complexidade e profundidade, sendo frequentemente comparadas a enigmas musicais.",
+        link: "https://www.ebiografia.com/johann_sebastian_bach/",
+        tags: "musico músico música compositor barroca"
+    },
+    {
+        titulo: "Wolfgang Amadeus Mozart",
+        descricao: "Wolfgang Amadeus Mozart (1756-1791): Um prodígio desde a infância, Mozart compôs uma vasta gama de obras com uma facilidade e elegância impressionantes. Sua música é caracterizada por melodias memoráveis e uma harmonia impecável.",
+        link: "https://pt.wikipedia.org/wiki/Wolfgang_Amadeus_Mozart",
+        tags: "mozart prodígio músico prodigio musico compositor "
+    },
+    {
+        titulo: "Frédéric Chopin",
+        descricao: "Frédéric Chopin (1810-1849): O pianista polonês é considerado um dos maiores virtuosos do piano. Suas composições são profundamente líricas e românticas, explorando as nuances e as possibilidades técnicas do instrumento.",
+        link: "https://www.infoescola.com/biografias/frederic-chopin/",
+        tags: "músico musico compositor clássico classico"
+    },
+    {
+        titulo: "Ludwig van Beethoven",
+        descricao: "Ludwig van Beethoven (1770-1827): Um dos compositores mais influentes da história, Beethoven expandiu os limites da música clássica, utilizando a orquestra de forma inovadora. Suas obras são marcadas por um forte drama e emoção.",
+        link: "https://www.todamateria.com.br/beethoven/",
+        tags: "músico musico compositor clássico classico"
+    },
+    {
+        titulo: "Piotr Ilyich Tchaikovsky",
+        descricao: "Piotr Ilyich Tchaikovsky (1840-1893): Um dos principais representantes do romantismo russo, Tchaikovsky é famoso por seus balés, como O Lago dos Cisnes e O Quebra-Nozes. Sua música é rica em melodias cativantes e paisagens sonoras exuberantes.",
+        link: "https://www.ebiografia.com/tchaikovsky/",
+        tags: "músico musico compositor clássico classico"
+    },
+];
